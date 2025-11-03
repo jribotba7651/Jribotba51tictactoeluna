32a1da6d324387a88af811282d77b7d3a2512cfe
Juan Ribot <jribot@gmail.com>
2025-11-02 18:19:11 -0800

    Initial Commit

---
+++ Jibaro_Tic_Tac_ToeTests/Jibaro_Tic_Tac_ToeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Jibaro_Tic_Tac_ToeTests.swift
+//  Jibaro_Tic_Tac_ToeTests
+//
+//  Created by Juan C. Ribot on 11/2/25.
+//
+
+import Testing
+@testable import Jibaro_Tic_Tac_Toe
+
+struct Jibaro_Tic_Tac_ToeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
